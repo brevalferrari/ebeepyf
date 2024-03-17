cef676f3ffcfdef62cdc7d1424aab802b50cd959
p6nj <breee@duck.com>
2024-03-17 17:30:00 +0100

    try hex display hint

--- ebeepyf-ebpf/src/main.rs
+++ ebeepyf-ebpf/src/main.rs
@@ -47,21 +47,31 @@ fn try_ebeepyf(ctx: XdpContext) -> Result<u32, ()> {
 
     let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
     let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+    let dest_addr = u32::from_be(unsafe { (*ipv4hdr).dst_addr });
 
-    let source_port = match unsafe { (*ipv4hdr).proto } {
+    let (source_port, dest_port) = match unsafe { (*ipv4hdr).proto } {
         IpProto::Tcp => {
             let tcphdr: *const TcpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
-            u16::from_be(unsafe { (*tcphdr).source })
+            (
+                u16::from_be(unsafe { (*tcphdr).source }),
+                u16::from_be(unsafe { (*tcphdr).dest }),
+            )
         }
         IpProto::Udp => {
             let udphdr: *const UdpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
-            u16::from_be(unsafe { (*udphdr).source })
+            (
+                u16::from_be(unsafe { (*udphdr).source }),
+                u16::from_be(unsafe { (*udphdr).dest }),
+            )
         }
         _ => return Err(()),
     };
 
     //
-    info!(&ctx, "SRC IP: {:i}, SRC PORT: {}", source_addr, source_port);
+    info!(
+        &ctx,
+        "{:X}:{}-{:X}:{}", source_addr, source_port, dest_addr, dest_port
+    );
 
     Ok(xdp_action::XDP_PASS)
 }
